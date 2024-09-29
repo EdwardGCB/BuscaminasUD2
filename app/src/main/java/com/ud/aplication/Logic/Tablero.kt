10417b21c61604437fd3a98a116201ac1ed900fd
Edward CB <egcastillob@udistrital.edu.co>
2024-09-29 18:28:28 -0500

    Agregacion de vista como bombas, reloj, banderas y explosiones

--- app/src/main/java/com/ud/aplication/Logic/Tablero.kt
+++ app/src/main/java/com/ud/aplication/Logic/Tablero.kt
@@ -6,12 +6,12 @@ import kotlin.random.Random
 
 object Tablero {
     private var tablero: MutableList<MutableList<Casilla>> = mutableListOf()
+    var minas: Int = 0
 
-    fun inicializarTablero(dificultad: String?): MutableList<MutableList<Casilla>>{
+    fun inicializarTablero(dificultad: String?): List<List<Casilla>>{
         if(tablero.isEmpty()){
             var filas =0
             var columnas =0
-            var minas = 0
             when(dificultad){
                 /*
                 * niveles:
@@ -54,8 +54,8 @@ object Tablero {
     * */
     fun mostrarCerosAdyacentes(f: Int,c: Int){
         tablero[f][c].estado = EnumEstado.VISIBLE.toString()
-        for (ft in (f-1).rangeUntil((f+1))){
-            for (ct in (c-1).rangeUntil((c+1))){
+        for (ft in (f-1)..((f+1))){
+            for (ct in (c-1)..((c+1))){
                 /*
                 * Realizamos una validacion donde se tienen que
                 * cumplir tres condiciones:
@@ -63,21 +63,24 @@ object Tablero {
                 * 2. la fila temporal o la columna tempotal deben ser
                 * diferentes a la fila y la columna que se selecciono
                 * 3. la cosilla tiene que estar oculta
+                * 4. que no sea una mina
                 * */
                 if(((ft>=0) && (ct>=0) && (ft< tablero.size) && (ct<tablero[0].size)) && ( (ft!=f) || (ct!=c))){
-                    if(tablero[ft][ct].estado != EnumEstado.VISIBLE.toString()){
-                        /*
-                        * Si cumple con las tres condiciones entonces hago la casilla visible
-                        * */
-                        tablero[ft][ct].estado = EnumEstado.VISIBLE.toString()
+                    if(tablero[ft][ct].valor != 100){
+                        if(tablero[ft][ct].estado != EnumEstado.VISIBLE.toString()){
+                            /*
+                            * Si cumple con las tres condiciones entonces hago la casilla visible
+                            * */
+                            tablero[ft][ct].estado = EnumEstado.VISIBLE.toString()
 
-                        /*
-                        * si cumple con la condicion de que el valor de la casilla
-                        * es 0 entonces repito el proceso pero con la posicion actual
-                        * */
+                            /*
+                            * si cumple con la condicion de que el valor de la casilla
+                            * es 0 entonces repito el proceso pero con la posicion actual
+                            * */
 
-                        if (tablero[ft][ct].valor == 0){
-                            mostrarCerosAdyacentes(ft,ct)
+                            if (tablero[ft][ct].valor == 0){
+                                mostrarCerosAdyacentes(ft,ct)
+                            }
                         }
                     }
                 }
@@ -92,7 +95,9 @@ object Tablero {
         for(i in 0 until tablero.size){
             for (j in 0 until tablero[0].size){
                 if (tablero[i][j].valor == 100){
-                    tablero[i][j].estado = EnumEstado.MINA_REVENTADA.toString()
+                    tablero[i][j].estado = EnumEstado.VISIBLE.toString()
+
+                    //tablero[i][j].estado = EnumEstado.MINA_REVENTADA.toString()
                 }
             }
         }
@@ -105,20 +110,28 @@ object Tablero {
 
     private fun agregarMinas(minas: Int){
         var contador = 0
-        while(contador --> minas){
+        while(contador < minas){
             val filaTemp = Random.nextInt(0,tablero.size)
             val columnaTemp = Random.nextInt(0,tablero[0].size)
             if (tablero[filaTemp][columnaTemp].valor != 100){
                 tablero[filaTemp][columnaTemp].valor = 100
-                contador++
                 for (ft2 in (filaTemp - 1)..(filaTemp+1)) {
                     for (ct2 in (columnaTemp-1)..(columnaTemp+1)){
-                        if (tablero[filaTemp][columnaTemp].valor != 100){
-                            tablero[filaTemp][columnaTemp].valor++
+                        if (ft2 >= 0 && ft2 < tablero.size && ct2 >= 0 && ct2 < tablero[0].size) {
+                            if (tablero[ft2][ct2].valor != 100) {
+                                tablero[ft2][ct2].valor++
+                            }
+
                         }
                     }
                 }
+                contador++
             }
         }
+    }
+
+    fun librerarEspacioMemoria(){
+        tablero= mutableListOf()
+        minas = 0
     }
 }
--- app/src/main/java/com/ud/aplication/MainActivity.kt
+++ app/src/main/java/com/ud/aplication/MainActivity.kt
@@ -5,41 +5,49 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ud.aplication.Enums.EnumDificultad
+import com.ud.aplication.Enums.EnumEstado
 import com.ud.aplication.Logic.Casilla
 import com.ud.aplication.Logic.Tablero
 
@@ -60,38 +68,69 @@ class MainActivity : ComponentActivity() {
 fun Ventana() {
     val dificultad = remember { mutableStateOf(EnumDificultad.MEDIUM.toString()) }
     var tablero = Tablero.inicializarTablero(dificultad.value)
+    val banderas = remember { mutableIntStateOf(0) }
+    banderas.value = Tablero.minas
+    val timer = remember { mutableIntStateOf(0) }
+    val refresh = remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
                 title = {
-                    Text("Buscaminas")
-                }
-
+                    TopInfo(timer, Tablero.minas, banderas)
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Default.Refresh, contentDescription = "Refresh")
+                    }
+                },
+                modifier = Modifier.padding(top = 6.dp)
             )
         },
         bottomBar = {
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(32.dp)
+                    .padding(4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
                 Desplegable(dificultad)
             }
         }
     ) { innerPadding ->
         Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-            MinesBoard(tablero)
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (refresh.value) {
+                tablero = Tablero.inicializarTablero(dificultad.value)
+                MinesBoard(tablero, refresh)
+            } else MinesBoard(tablero = Tablero.inicializarTablero(dificultad.value), refresh)
         }
+    }
+}
+
+@Composable
+fun TopInfo(timer: MutableState<Int>, minas: Int, banderas: MutableState<Int>) {
+    val imageReloj = R.drawable.reloj
+    val imageMinies = R.drawable.bomb
+    val imageFlag = R.drawable.bandera
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(painterResource(imageMinies), contentDescription = "Minas")
+        Text(minas.toString())
+        Spacer(modifier = Modifier.width(20.dp))
+        Image(painterResource(imageReloj), contentDescription = "Reloj")
+        Text(timer.value.toString())
+        Spacer(modifier = Modifier.width(20.dp))
+        Image(painterResource(imageFlag), contentDescription = "Banderas")
+        Text(banderas.value.toString())
+
     }
 }
 
@@ -104,6 +143,7 @@ fun Desplegable(seleccion: MutableState<String>) {
 
     ExposedDropdownMenuBox(
         expanded = desplegar.value,
+        modifier = Modifier.width(160.dp),
         onExpandedChange = {
             desplegar.value = !desplegar.value
         }
@@ -128,6 +168,7 @@ fun Desplegable(seleccion: MutableState<String>) {
                         seleccion.value = item
                         desplegar.value = false
                         Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                        Tablero.librerarEspacioMemoria()
                     }
                 )
             }
@@ -137,30 +178,81 @@ fun Desplegable(seleccion: MutableState<String>) {
 }
 
 @Composable
-fun MinesBoard (tablero: MutableList<MutableList<Casilla>>){
-    for(f in 0 until tablero.size){
+fun MinesBoard(tablero: List<List<Casilla>>, refresh: MutableState<Boolean>) {
+    for (f in tablero.indices) {
         Row {
-            for(c in 0 until tablero[0].size){
-                MiniesButton(f,c,tablero[f][c])
+            for (c in 0 until tablero[0].size) {
+                when (tablero[f][c].estado) {
+                    EnumEstado.VISIBLE.toString() -> MiniesBox(tablero[f][c])
+                    EnumEstado.OCULTA.toString() -> MiniesButton(f, c, tablero[f][c], refresh)
+                }
             }
         }
     }
 }
 
 @Composable
-fun MiniesButton(f: Int, c: Int, casilla: Casilla){
-    val clicked = remember{ mutableStateOf(false) }
-    Button(
-        onClick = {
-            clicked.value = true },
+fun MiniesButton(f: Int, c: Int, casilla: Casilla, refresh: MutableState<Boolean>) {
+    if (casilla.estado == EnumEstado.MINA_MARCADA.toString()) {
+        ImageButton(R.drawable.bandera)
+    }else {
+        Button(
+            onClick = {
+                /*
+                * Cuando clickeo el boton tengo tres posibilidades:
+                * 1. si el valor es cero entonces muestro los adyacentes
+                * 2. si el valor es una mina = 100 entonces muestro todas las minas y acabo el juego
+                * 3. si el valor es cualquier otro numero, solo lo muestro
+                * */
+                when (casilla.valor) {
+                    0 -> Tablero.mostrarCerosAdyacentes(f, c)
+                    100 -> Tablero.mostrarMinas()
+                    else -> casilla.estado = EnumEstado.VISIBLE.toString()
+                }
+                refresh.value = !refresh.value
+            },
+            modifier = Modifier
+                .padding(4.dp)
+                .size(40.dp),
+            shape = RoundedCornerShape(0.dp)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun MiniesBox(casilla: Casilla) {
+    if(casilla.valor == 100){
+        ImageButton(R.drawable.explosion)
+    }else{
+        Box(
             modifier = Modifier
                 .padding(4.dp)
                 .size(40.dp)
-
+                .border(2.dp, Color.DarkGray),
+            contentAlignment = Alignment.Center
         ) {
-        Text(
-            text = casilla.valor.toString(),
-            modifier = Modifier.fillMaxSize()
-        )
+            Text(
+                text = if(casilla.valor == 0)"" else casilla.valor.toString(),
+                fontSize = 16.sp,
+                modifier = Modifier.fillMaxSize(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
+}
+
+@Composable
+fun ImageButton(imageId: Int) {
+
+    // Crea un botón con la imagen
+    Image(
+        painter = painterResource(imageId),
+        contentDescription = null, // Puedes agregar una descripción aquí
+        modifier = Modifier
+            .padding(4.dp)
+            .size(40.dp)
+            .clickable { /* Acción al hacer clic en la imagen */ },
+    )
 }
